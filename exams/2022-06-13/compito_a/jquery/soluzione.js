22b2a94b28c62e04aa007829cf4e47f1361203df
Nickolausen <nicholas.magi24@gmail.com>
2026-01-28 13:37:10 +0100

    practising exams

---
+++ exams/2022-06-13/compito_a/jquery/soluzione.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const form = document.querySelector("form")
+    const btn = document.querySelectorAll("button").entries()
+    console.log("Here")
+})
